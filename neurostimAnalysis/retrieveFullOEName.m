d2b8bd07aabb74370f03444e0378b13df4799189
dshimaoka <daisuke.shimaoka@monash.edu>
2022-10-28 13:02:58 +1100

    go back to original dir

--- neurostimAnalysis/retrieveFullOEName.m
+++ neurostimAnalysis/retrieveFullOEName.m
@@ -1,8 +1,8 @@
 function fullOEName = retrieveFullOEName(pth, partialOEName)
 % Open ephys session folders are names as follows:
-% 
+%
 %   [pth]/[prefix_]yyyy-mm-dd_HH-MM-SS[_suffix]/
-% 
+%
 % On the marmolab rigs this means they should follow the naming convention:
 %
 %   [pth]/subject.paradigm.HHMMSS_yyyy-mm-dd_HH-MM-SS/
@@ -27,20 +27,24 @@ pinfo = regexp(pth,pat,'names');
 
 assert(~isempty(pinfo),'Supplied path should match: [blah]/yyyy/mm/dd/')
 
-wd = cd(pth);
-
-d = dir(fullfile(pth, [partialOEName '*']));
-
-% keep only the folders/directories
-d(~[d.isdir]) = [];
-
-pat = '^(?<subject>.+)\.(?<paradigm>.+)\.(?<hour>\d{2})(?<minute>\d{2})(?<second>\d{2})$'; % <subject>.<paradigm>.<timestamp>
-m = regexp({d.name},pat,'names');
-
-ii = 1;
-src = d(ii).name;
-fullOEName = src;
-% fullOEName = sprintf('%s_%s-%s-%s_%s-%s-%s', ...
-%     d(ii).name,pinfo.year,pinfo.month,pinfo.day,m{ii}.hour,m{ii}.minute,m{ii}.second);
-
-cd(currentDir);
+try
+    wd = cd(pth);
+    
+    d = dir(fullfile(pth, [partialOEName '*']));
+    
+    % keep only the folders/directories
+    d(~[d.isdir]) = [];
+    
+    pat = '^(?<subject>.+)\.(?<paradigm>.+)\.(?<hour>\d{2})(?<minute>\d{2})(?<second>\d{2})$'; % <subject>.<paradigm>.<timestamp>
+    m = regexp({d.name},pat,'names');
+    
+    ii = 1;
+    src = d(ii).name;
+    fullOEName = src;
+    % fullOEName = sprintf('%s_%s-%s-%s_%s-%s-%s', ...
+    %     d(ii).name,pinfo.year,pinfo.month,pinfo.day,m{ii}.hour,m{ii}.minute,m{ii}.second);
+    
+    cd(currentDir);
+catch err
+    cd(currentDir);
+end
